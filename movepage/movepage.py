b0b359b865e75369aaf0afddbce8281c528c3fca
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-06-18 14:14:45 -0500

    rename to move, and fix redirect location name

--- movepage/movepage.py
+++ movepage/movepage.py
@@ -12,13 +12,13 @@ class MovePage(commands.Cog):
 		self.summary = 'Moving page + associated subpages'
 
 	@commands.group(pass_context=True)
-	async def movepage(self, ctx, wiki, p1, p2):
+	async def move(self, ctx, wiki, p1, p2):
 		site = await utils.login_if_possible(ctx, self.bot, wiki)
 		if site is None:
 			return
 		p1 = p1[0].upper() + p1[1:]
 		for page in site.client.allpages(prefix=p1 + '/'):
-			dest = page.name.replace(p1 + '/', p2)
+			dest = page.name.replace(p1 + '/', p2 + '/')
 			await ctx.send("Moving page {} to {}".format(page.name, dest))
 			page.move(dest)
 		
