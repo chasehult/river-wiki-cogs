5fa225b11aecd9bb5ce78df77b1910bbceb932b4
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-07-13 13:09:21 -0500

    add docstrings

--- menascrape/menascrape.py
+++ menascrape/menascrape.py
@@ -17,6 +17,7 @@ class MenaScrape(commands.Cog):
 	
 	@menascrape.command()
 	async def create(self, ctx, *, title):
+		"""Creates MatchSchedule code that you can then copy to Data namespace"""
 		site = await utils.login_if_possible(ctx, self.bot, 'lol')
 		await ctx.send('Okay, starting now!')
 		page_updated = MenaCreator(site, title).run()
@@ -24,6 +25,7 @@ class MenaScrape(commands.Cog):
 	
 	@menascrape.command()
 	async def update(self, ctx, *, title):
+		"""Updates a live Data namespace page in place"""
 		site = await utils.login_if_possible(ctx, self.bot, 'lol')
 		await ctx.send('Okay, starting now!')
 		page_updated = MenaUpdater(site, title).run()
