740d6595d428f173f59527be2d5c7c509533c8ad
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-07-13 12:31:45 -0500

    move stuff to other library

--- menascrape/menascrape.py
+++ menascrape/menascrape.py
@@ -1,6 +1,7 @@
 from redbot.core import commands
 import rivercogutils as utils
-from .menascrape_main import MenaScraper
+from toornament_scraper.mena_creator import MenaCreator
+from toornament_scraper.mena_updater import MenaUpdater
 
 
 class MenaScrape(commands.Cog):
@@ -11,8 +12,13 @@ class MenaScrape(commands.Cog):
 		self.summary = 'Moving page + associated subpages'
 
 	@commands.command()
-	async def menascrape(self, ctx, title):
+	async def menascrape(self, ctx, action, title):
 		site = await utils.login_if_possible(ctx, self.bot, 'lol')
 		await ctx.send('Okay, starting now!')
-		MenaScraper(site, title).run()
+		if action == 'update':
+			MenaUpdater(site, title).run()
+		elif action == 'create':
+			MenaCreator(site, title).run()
+		else:
+			await ctx.send('Sorry, unknown action!')
 		await ctx.send('Okay, done!')
