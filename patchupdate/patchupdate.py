0f59a6f0b7ed379839f8a782325600c07c032a64
AA <chasehult@gmail.com>
2021-04-02 07:52:54 -0700

    No more lambdas

--- patchupdate/patchupdate.py
+++ patchupdate/patchupdate.py
@@ -2,7 +2,7 @@ import asyncio
 import json
 import re
 from functools import wraps, partial
-from typing import Optional
+from typing import Optional, Type, Callable, List
 
 import aiohttp
 import rivercogutils as utils
@@ -13,12 +13,35 @@ from river_mwclient.wiki_client import WikiClient
 DDRAGON_V = "https://ddragon.leagueoflegends.com/api/versions.json"
 DDRAGON = "http://ddragon.leagueoflegends.com/cdn/{}/data/en_US/{}.json"
 
-restrip = lambda x: re.sub(r'[^A-Za-z]', '', capspace(x.strip()))
-capfirst = lambda x: re.sub(r'^.', lambda x: x.group().upper(), x)
-capspace = lambda x: re.sub(r'\s.|^.', lambda x: x.group().upper(), x.lower())
-ifinelse = lambda d, k: d[k] if k and k in d else ''
-strperc = lambda i: '' if not i else str(int(float(i) * 100)) + '%' if float(i * 100) % 1 == 0 else str(
-    float(i * 100)) + '%'
+
+def capspace(s):
+    """Capitalize each word in a string"""
+    return re.sub(r'\s.|^.', lambda x: x.group().upper(), s.lower())
+
+
+def restrip(s):
+    """Remove non-alpha characters from a string"""
+    return re.sub(r'[^A-Za-z]', '', capspace(s.strip()))
+
+
+def capfirst(s):
+    """Capitalize the very first character of a string"""
+    return re.sub(r'^.', lambda x: x.group().upper(), s)
+
+
+def ifinelse(d, k):
+    """Return d[k] only if d exists in k, otherwise return an empty string"""
+    return d[k] if k and k in d else ''
+
+
+def strperc(i):
+    """Nicely turn a decimal into a percent"""
+    if not i:
+        return ''
+    elif float(i * 100) % 1 == 0:
+        return str(int(float(i) * 100)) + '%'
+    else:
+        return str(float(i * 100)) + '%'
 
 
 def async_wrap(func):
@@ -28,103 +51,70 @@ def async_wrap(func):
             loop = asyncio.get_event_loop()
         pfunc = partial(func, *args, **kwargs)
         return await loop.run_in_executor(executor, pfunc)
-    
+
     return run
 
+
 class Formatter:
-    @classmethod
-    def format(cls, d):
+    def __init__(self, data):
+        self.data = data
+
+    def format(self):
         raise NotImplementedError()
 
-    @classmethod
-    def extras(cls, d, t):
-        return
 
 class ChampionFormatter(Formatter):
-    @classmethod
-    def format(cls, d):
+    def format(self):
         return {
-            'name': d['name'],
-            'title': capfirst(d['title']),
+            'name': self.data['name'],
+            'title': capfirst(self.data['title']),
 
-            'key_int': d['key'],
+            'key_int': self.data['key'],
 
-            'resource': d['partype'],
-            'attribute': d['tags'][0],
-            'attribute2': d['tags'][1] if len(d['tags']) > 1 else '',
+            'resource': self.data['partype'],
+            'attribute': self.data['tags'][0],
+            'attribute2': self.data['tags'][1] if len(self.data['tags']) > 1 else '',
 
-            'hp': d['stats']['hp'],
-            'hp_lvl': d['stats']['hpperlevel'],
-            'hpregen': d['stats']['hpregen'],
-            'hpregen_lvl': d['stats']['hpregenperlevel'],
-            'mana': d['stats']['mp'] if d['partype'] == 'Mana' else '',
-            'mana_lvl': d['stats']['mpperlevel'] if d['partype'] == 'Mana' else '',
-            'mregen': d['stats']['mpregen'] if d['partype'] == 'Mana' else '',
-            'mregen_lvl': d['stats']['mpregenperlevel'] if d['partype'] == 'Mana' else '',
-            'range': d['stats']['attackrange'],
-            'ad': d['stats']['attackdamage'],
-            'ad_lvl': d['stats']['attackdamageperlevel'],
-            'as': d['stats']['attackspeed'],
-            'as_lvl': d['stats']['attackspeedperlevel'],
-            'armor': d['stats']['armor'],
-            'armor_lvl': d['stats']['armorperlevel'],
-            'mr': d['stats']['spellblock'],
-            'mr_lvl': d['stats']['spellblockperlevel'],
-            'ms': d['stats']['movespeed'],
-
-            'extras': cls.extras,
+            'hp': self.data['stats']['hp'],
+            'hp_lvl': self.data['stats']['hpperlevel'],
+            'hpregen': self.data['stats']['hpregen'],
+            'hpregen_lvl': self.data['stats']['hpregenperlevel'],
+            'mana': self.data['stats']['mp'] if self.data['partype'] == 'Mana' else '',
+            'mana_lvl': self.data['stats']['mpperlevel'] if self.data['partype'] == 'Mana' else '',
+            'mregen': self.data['stats']['mpregen'] if self.data['partype'] == 'Mana' else '',
+            'mregen_lvl': self.data['stats']['mpregenperlevel'] if self.data['partype'] == 'Mana' else '',
+            'range': self.data['stats']['attackrange'],
+            'ad': self.data['stats']['attackdamage'],
+            'ad_lvl': self.data['stats']['attackdamageperlevel'],
+            'as': self.data['stats']['attackspeed'],
+            'as_lvl': self.data['stats']['attackspeedperlevel'],
+            'armor': self.data['stats']['armor'],
+            'armor_lvl': self.data['stats']['armorperlevel'],
+            'mr': self.data['stats']['spellblock'],
+            'mr_lvl': self.data['stats']['spellblockperlevel'],
+            'ms': self.data['stats']['movespeed'],
         }
 
 
 class ItemFormatter(Formatter):
-    @classmethod
-    def format(cls, d):
+    def format(self):
         return {
-            'name': d['name'],
+            'name': self.data['name'],
             'item_code': None,
 
-            'ad': ifinelse(d['stats'], 'FlatPhysicalDamageMod'),
-            'ls': strperc(ifinelse(d['stats'], 'PercentLifeStealMod')),
-            'hp': ifinelse(d['stats'], 'FlatHPPoolMod'),
-            'hpregen': ifinelse(d['stats'], 'FlatHPRegenMod'),
-            'armor': ifinelse(d['stats'], 'FlatArmorMod'),
-            'mr': ifinelse(d['stats'], 'FlatSpellBlockMod'),
-            'crit': ifinelse(d['stats'], 'FlatCritChanceMod'),
-            'as': strperc(ifinelse(d['stats'], 'PercentAttackSpeedMod')),
-            # 'armorpen': ifinelse(d['stats'], ''),
-            # 'range': ifinelse(d['stats'], ''),
-            # 'mana': ifinelse(d['stats'], ''),
-            # 'manaregen': ifinelse(d['stats'], ''),
-            # 'energy': ifinelse(d['stats'], ''),
-            # 'energyregen': ifinelse(d['stats'], ''),
-            # 'ap': ifinelse(d['stats'], ''),
-            # 'cdr': ifinelse(d['stats'], ''),
-            # 'spellvamp': ifinelse(d['stats'], ''),
-            # 'magicpen': ifinelse(d['stats'], ''),
-            # 'ms': ifinelse(d['stats'], ''),
-            # 'tenacity': ifinelse(d['stats'], ''),
-            # 'goldgen': ifinelse(d['stats'], ''),
-            # 'onhit': ifinelse(d['stats'], ''),
-            # 'bonushp': ifinelse(d['stats'], ''),
-            # 'healing': ifinelse(d['stats'], ''),
+            'ad': ifinelse(self.data['stats'], 'FlatPhysicalDamageMod'),
+            'ls': strperc(ifinelse(self.data['stats'], 'PercentLifeStealMod')),
+            'hp': ifinelse(self.data['stats'], 'FlatHPPoolMod'),
+            'hpregen': ifinelse(self.data['stats'], 'FlatHPRegenMod'),
+            'armor': ifinelse(self.data['stats'], 'FlatArmorMod'),
+            'mr': ifinelse(self.data['stats'], 'FlatSpellBlockMod'),
+            'crit': ifinelse(self.data['stats'], 'FlatCritChanceMod'),
+            'as': strperc(ifinelse(self.data['stats'], 'PercentAttackSpeedMod')),
+            'totalgold': self.data['gold']['total'],
+            'sold': self.data['gold']['sell'],
 
-            'totalgold': d['gold']['total'],
-            'sold': d['gold']['sell'],
-
-            'extras': cls.extras,
+            'used_in': ','.join(self.data['into'])
         }
-
-    @classmethod
-    def extras(cls, d, t):
-        usedin = []
-        index = 1
-        while t.has("used in " + str(index)):
-            usedin.append(t.get("used in " + str(index)).value.strip())
-            index += 1
-        t.add("used_in", ','.join(usedin))
-
-
-SPEC_ITEMS = ['extras']
 
 
 class TemplateModifier(TemplateModifierBase):
@@ -138,43 +128,40 @@ class TemplateModifier(TemplateModifierBase):
                          limit=limit, summary=summary, quiet=quiet, lag=lag, tags=tags,
                          skip_pages=skip_pages,
                          startat_page=startat_page)
-    
+
     @async_wrap
     def fakesync_run(self):
         self.run()
-    
+
     def update_template(self, template):
-        key = [k for k,v in self.data.items() if v.get('name') == template.get('name', '').value.strip()]
-        if len(key)==1:
+        key = [k for k, v in self.data.items() if v.get('name') == template.get('name', '').value.strip()]
+        if len(key) == 1:
             template.add('ddragon_key', key[0])
-            data = self.formatter.format(self.data.get(key[0]))
+            data = self.formatter(self.data.get(key[0])).format()
         else:
             self.site.log_error_content(self.current_page.name, "Duplicate or missing DDragon data")
             return
 
         for key, value in data:
-            if key in SPEC_ITEMS:
-                continue
             if str(value):
                 template.add(key, str(value))
             else:
                 if template.has(key) and not template.get(key).value.strip():
                     template.remove(key, True)
-        for spec in SPEC_ITEMS:
-            if spec in data:
-                data[spec](self.data, template)
+        for meth in self.formatter.EXTRA_METHODS:
+            meth(self.data, template)
 
 
 class PatchUpdate(commands.Cog):
     def __init__(self, bot):
         super().__init__()
         self.bot = bot
-    
+
     @commands.group()
     async def patchupdate(self, ctx):
         pass
-    
-    async def updatestats(self, ctx, version: Optional[str], section: str, formatter: Formatter):
+
+    async def updatestats(self, ctx, version: Optional[str], section: str, formatter: Type[Formatter]):
         async with aiohttp.ClientSession() as session, ctx.typing():
             if version is None:
                 async with session.get(DDRAGON_V) as resp:
@@ -190,11 +177,11 @@ class PatchUpdate(commands.Cog):
             await tm.fakesync_run()
             site.report_all_errors('patchupdate')
         await ctx.send("Okay, done!")
-    
+
     @patchupdate.command()
     async def championstats(self, ctx, version=None):
         await self.updatestats(ctx, version, 'Champion', ChampionFormatter)
-    
+
     @patchupdate.command()
     async def itemstats(self, ctx, version=None):
         await self.updatestats(ctx, version, 'Item', ItemFormatter)
